42e25e3a2d91f653f5184c1a848a8f11174e1a5f
Andrei Lucas <andrei_rheivjarth@hotmail.com>
2025-06-04 19:46:22 -0300

    Criado classe usuario e templete home

---
+++ src/main/java/br/com/dobackaofront/cadastro/FormularioComSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.dobackaofront.cadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormularioComSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormularioComSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dobackaofront/cadastro/FormularioComSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dobackaofront.cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormularioComSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
